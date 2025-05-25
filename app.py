bc805117d111822bb17366c6a639272df856daa5
zabih-cpu <khanzabih272@gmail.com>
2025-05-25 02:43:53 -0700

    add new code to git hub

--- app.py
+++ app.py
@@ -7,6 +7,35 @@
 # sum = a+b
 # print("sum of a and b is :",sum)
 
+# arithmetic operater
+# a = 5
+# b = 4
+# print(a+b)
+# print(a-b)
+# print(a/b)
+# print(a*b)
+# # print(a % b) use for reminder
 
 
+# relational/comparison operater
+
+# a = 40
+# b = 10
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a < b)
+# print(a <= b)
+# print(a >= b)
+
+#assignment operater 
+
+# num = 10
+# num += 20
+# num -= 20
+# num *= 20
+# num /= 20
+# num %= 20
+# print(num)
+ 
 
