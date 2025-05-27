b5fe0de764717e12cfe392edf99ad875df5d341f
zabih-cpu <khanzabih272@gmail.com>
2025-05-27 04:15:05 -0700

    add new practice question

--- app.py
+++ app.py
@@ -55,13 +55,31 @@
 
 # Input in python 
 
-name = input("enter your name :")
-age = float(input("enter your age :"))
-marks = int (input("enter your marks :"))
+# name = input("enter your name :")
+# age = float(input("enter your age :"))
+# marks = int (input("enter your marks :"))
 
-print(name)
-print(type(age),age)
-print(type(marks),marks)
+# print(name)
+# print(type(age),age)
+# print(type(marks),marks)
+
+# practice question (write a parogram to input two number and print sum)
+# first = int(input("enter first number :"))
+# second = int(input("enter second number :"))
+
+# print("sum :",first + second)
+
+# practice questuon (write a parogram to input a size of square and print the area )
+# side = float(input("area are :"))
+
+# print(side*side)
+
+# # practice question (write a parograme input 2 floating point number and print thier average)
+# float1 = float(input("first number :"))
+# float2 = float(input("second number :"))
+
+
+# print("average is :", (float1 * float2)/2)
 
 
 
